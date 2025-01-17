2e2a52e54d340f6f4b34c984511199aa783675fa
darkRihito <fieshidiq@gmail.com>
2025-01-17 19:51:21 +0700

    Deployment bug fixes 4

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,8 +6,8 @@ import {
   Inter,
 } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/app/components/Navbar";
-import Footer from "@/app/components/Footer";
+// import Navbar from "@/app/components/Navbar";
+// import Footer from "@/app/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",

e5e2a88ffd95388046d59505b8957d3e325a1ecb
darkRihito <fieshidiq@gmail.com>
2025-01-17 20:30:46 +0700

    Deployment bug fixes 13

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -6,14 +6,14 @@ const Navbar = () => {
     <>
       <div className="fixed top-0 left-0 w-full z-50">
         <div className="relative mx-auto w-fit bg-neutral-800/60 backdrop-blur-sm rounded-lg mt-8 px-3 text-sm md:text-base">
-          <ul className="flex items-center gap-4">
-            <a href="/" className="py-3 px-2 font-rethink transition-colors">
+          <ul className="flex items-center gap-4 font-rethink">
+            <a href="/" className="py-3 px-2 transition-colors">
               Career
             </a>
-            <a href="/atelier" className="py-3 px-2 font-rethink transition-colors">
+            <a href="/atelier" className="py-3 px-2 transition-colors">
               Atelier
             </a>
-            <a href="/" className="py-3 px-2 font-rethink transition-colors">
+            <a href="/" className="py-3 px-2 transition-colors">
               Caleidoscope
             </a>
           </ul>
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,7 +6,7 @@ import {
   Inter,
 } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/app/components/Navbar";
+// import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
 
 const geistSans = Geist({
@@ -41,7 +41,21 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${rethinkSans.variable} ${passionsConflict.variable} antialiased`}
       >
-        <Navbar />
+        <div className="fixed top-0 left-0 w-full z-50">
+          <div className="relative mx-auto w-fit bg-neutral-800/60 backdrop-blur-sm rounded-lg mt-8 px-3 text-sm md:text-base">
+            <ul className="flex items-center gap-4 font-rethink">
+              <a href="/" className="py-3 px-2 transition-colors">
+                Career
+              </a>
+              <a href="/atelier" className="py-3 px-2 transition-colors">
+                Atelier
+              </a>
+              <a href="/" className="py-3 px-2 transition-colors">
+                Caleidoscope
+              </a>
+            </ul>
+          </div>
+        </div>
         {children}
         <Footer />
       </body>
